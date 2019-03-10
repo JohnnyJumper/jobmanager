3eda3422f7f6185c7903ab9c765fe2ea58e3c4fb
Jeyhun Tahirov <jtahirov@e1z2r9p28.42.us.org>
2019-03-09 23:51:18 -0800

    modals clicks from

---
+++ client/src/modal.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom';
+import {Button} from 'reactstrap';
+
+const modalRoot = document.querySelector("#modal");
+
+export default class Modal extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			control: props.control
+		};
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		if (prevProps.control != this.props.control) {
+			console.log({prevProps, prevState});
+			return this.setState({control: this.props.control});
+		}
+	}
+    render() {
+        const {control} = this.state;
+		if (control){
+            return ReactDOM.createPortal(
+                    <div className="modal-wrapper-outside">
+                        <div className="modal-wrapper-inside">
+                            {this.props.children}
+                            <Button onClick={this.props.toggle} color="info">Close</Button>
+                        </div>
+                    </div>
+				, modalRoot);
+		}
+        return null;
+    }
+}
--- client/src/pages/View/View.js
+++ client/src/pages/View/View.js
@@ -4,8 +4,9 @@ import {Container} from 'reactstrap';
 import { Query } from 'react-apollo';
 import {getCompanies, getJobs, getInterviews} from '../../queries/queries';
 
+import Modal from '../../modal';
 const Mark = ({responded}) => (
-    responded ? (<svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>) : 'NOT'
+    responded ? (<svg className="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle className="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path className="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>) : 'NOT'
 )
 
 export const Loading = () => (
@@ -24,8 +25,11 @@ class View extends Component {
 
         this.state = {
             view: {},
+			modalStatus: false,
+			selected: {}
         }
-        this.renderView = this.renderView.bind(this);
+		this.renderView = this.renderView.bind(this);
+		this.toggleModal = this.toggleModal.bind(this);
         this.queries = {
                 jobs: getJobs,
                 companies: getCompanies,
@@ -33,23 +37,24 @@ class View extends Component {
             };
     }
 
-
+    toggleModal() {
+        this.setState(PrevState => ({modalStatus: !PrevState.modalStatus}));
+    }
 	
     renderJobs(data) {
-        console.log('data!', data);
-        return data.data.jobs.map(job => <li className="view-li" key={job.id}>
+        return data.data.jobs.map(job => <li onClick={() => this.setState({selected: {...job}}, this.toggleModal)}  className="view-li" key={job.id}>
                <span> {job.title} at {job.company.name}</span> <Mark responded={job.responded}/>
                 </li>);
     }
 
     renderInterviews(data) {
-        return data.data.interviews.map(interview => <li className="view-li" key={interview.id}>
+        return data.data.interviews.map(interview => <li onClick={() => this.setState({selected: {...interview}}, this.toggleModal)}  className="view-li" key={interview.id}>
         <span> {interview.type} at {interview.job.company.name} for {interview.job.title} </span>
         </li>)
     }
 
     renderCompanies(data) {
-        return data.data.companies.map(company => <li className="view-li" key={company.id}>
+        return data.data.companies.map(company => <li onClick={() => this.setState({selected: {...company}}, this.toggleModal)}  className="view-li" key={company.id}>
         <span>{company.name} applied: {company.applied}</span>
         </li>)
     }
@@ -67,7 +72,38 @@ class View extends Component {
                     return this.renderInterviews(props);
             }
         }
-  
+
+	renderJobModal() {
+		const {selected} = this.state;
+		if (typeof selected.title === 'undefined') return null;
+		return <span> {selected.title} at {selected.company.name}</span>;
+	}
+
+	renderCompanyModal() {
+		const {selected} = this.state;
+		if (typeof selected.name === 'undefined') return null;
+		return <span>{selected.name} applied: {selected.applied}</span>;
+	}
+
+	renderInterviewModal() {
+		const {selected} = this.state;
+		if (typeof selected.type === 'undefined') return null;
+		return <span> {selected.type} at {selected.job.company.name} for {selected.job.title} </span>;
+	}
+
+	renderModal() {
+		const {match: {params: {category: category} }} = this.props;
+		switch(category) {
+			case 'jobs': 
+				return this.renderJobModal();
+			case 'companies':
+				return this.renderCompanyModal();
+			case 'interviews':
+				return this.renderInterviewModal();
+		}
+	}		
+
+
     render() {
         const {match: {params: {category: category} }} = this.props;
         return (
@@ -77,6 +113,9 @@ class View extends Component {
                         {this.renderView}
                     </Query>
                 </ol>
+                <Modal control={this.state.modalStatus} toggle={this.toggleModal}>
+					{this.renderModal()}
+                </Modal>
             </Container>
         )
     }
